3cdd70ec2595c268fbbac10ec4d829aab67e516c
Duke Yoo <dukuaris@gmail.com>
2020-01-22 16:10:03 +0900

    Developing editing contents

--- ch99/bookmark/urls.py
+++ ch99/bookmark/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
-from bookmark.views import BookmarkLV, BookmarkDV
+# from bookmark.views import BookmarkLV, BookmarkDV 삭제 대신에 아래 views.py 모듈자체를 import
+from bookmark import views
+
 
 app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkLV.as_view(), name='index'),
     path('<int:pk>', BookmarkDV.as_view(), name='detail'),
+
+    # Example: /bookmark/add/
+    path('add/', views.BookmarkCreateView.as_view(), name="add"),
+
+    # Example: /bookmark/change/
+    path('change/', viewsBookmarkChangeLV.as_view(), name="change"),
+
+    # Example: /bookmark/99/update/
+    path('<ing:pk>/update', views.BookmarkUpdateView.as_view(), name="update"),
+
+    # Example: /bookmark/99/delete/
+    path('<int:pk>/delete/', views.BookmarkDeleteView.as_view(), name="delete"),
+
 ]
